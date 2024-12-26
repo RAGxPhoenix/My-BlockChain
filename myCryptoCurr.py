c4b7b9735ade2fd00d3bf314bd49b8b0a8ee19bd
Anik B <141936601+RAGxPhoenix@users.noreply.github.com>
2024-12-26 18:30:04 +0530

    Update myCryptoCurr.py

--- myCryptoCurr.py
+++ myCryptoCurr.py
@@ -13,11 +13,14 @@ def updateHash(*args):
 class Block:
     def __init__(self, data, block_number=0):
         self.data = data
-        self.timestamp = time.time()  # Instance variable for unique timestamp
+        self.timestamp = time.time()
         self.nonce = 0
         self.block_number = block_number
         self.previous_hash = "0" * 64
-
+        
+    def return_time_of_block(self):
+        return (self.timestamp)
+    
     def hash(self):
         return updateHash(self.block_number, self.timestamp, self.nonce, self.data, self.previous_hash)
 
@@ -32,25 +35,18 @@ class Block:
         )
 
 class Blockchain:
-    difficulty = 6 #this mainly decide how much difficult or complex the block would be to mine
+    difficulty = 5
 
     def __init__(self):
         self.chain = []
 
     def add(self, block):
-        self.chain.append({
-            'Block Number': block.block_number,
-            'Time Stamp': block.timestamp,
-            'Nonce': block.nonce,
-            'Data': block.data,
-            'Previous Hash': block.previous_hash,
-            'Current Hash': block.hash()
-        })
+        self.chain.append(block)
 
     def mine(self, block):
         # Update the block's previous_hash to the last block's hash in the chain
         if self.chain:
-            block.previous_hash = self.chain[-1]['Current Hash']
+            block.previous_hash = self.chain[-1].hash()
 
         # Proof-of-work: find a nonce that produces the required difficulty
         while True:
@@ -60,26 +56,40 @@ class Blockchain:
             else:
                 block.nonce += 1
 
+    def isValid(self):
+        for hash in range(1,len(self.chain)):
+            previous = self.chain[hash].previous_hash
+            current = self.chain[hash-1].hash()
+
+            if previous != current or current[:self.difficulty] != '0' * self.difficulty:
+                return False
+                    
+        return True            
+
 def main():
-    print(f"Start time is {time.time()}")
+    a = time.time()
+    print(f"Start time is {a}")
+
     my_block_chain = Blockchain()
     datas = ["Transaction1", "Transaction2", "Transaction3", "Transaction4"]
-
+    times = []
     for num, data in enumerate(datas, start=1):
         block = Block(data, num)
         my_block_chain.mine(block)
+        times.append(block.return_time_of_block())
 
-    for i, block_dict in enumerate(my_block_chain.chain, start=1):
-        # Convert the dictionary back to Block object for readable output
-        block = Block(
-            data=block_dict['Data'],
-            block_number=block_dict['Block Number']
-        )
-        block.timestamp = block_dict['Time Stamp']
-        block.nonce = block_dict['Nonce']
-        block.previous_hash = block_dict['Previous Hash']
-        print(f"Block {i}:\n{block}")
+    #my_block_chain.chain[1].data = 'Transaction2'  #toggle the data field to see the change in verfication
 
+    for i in my_block_chain.chain:
+        print(i)    
+        
+    x = 0 
+    print("Total time taken after each block is mined:  ")
+    for i in times :
+        x = i-a + x
+        print(x)
+
+    print(f"\nThe validity of the BlockChain : {my_block_chain.isValid()}\n")
 
 if __name__ == '__main__':
     main()
